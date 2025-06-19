374e33417c73de3f7899fdfc27fd536b7bb3f515
sabts <sabrinataraschi@gmail.com>
2025-06-19 11:36:20 +0200

    .

---
+++ client/src/components/profile/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+	return (
+		<>
+			<div>es un img pero eso aun no existe</div>
+			<h2>User Name</h2>
+			<div>
+				<strong>email:</strong>
+				<span>email@gmail.com</span>
+			</div>
+			<button>Edit</button>
+		</>
+	);
+};
---
+++ client/src/lib/utils/api.js
@@ -0,0 +1,2 @@
+const URL_BASE = 'http://localhost:3000';
+const URL_API = '/api/users';
